bbd50094bd07558c6e33d6750ed5054d1a0b8d3e
Daniyal <daniyalnaeem5@gmail.com>
2020-09-11 00:18:41 +0500

    props

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello {this.props.name}</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
+
